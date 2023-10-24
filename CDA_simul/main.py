13d8bce6d169105061a998e14e2b7030a9826cb3
ShinkyungLee <102726679+ShinkyungLee@users.noreply.github.com>
2023-10-24 10:29:40 +0900

    Update main.py : LICENSE 확인
    GNU LICENSE 적용됩니다.

--- CDA_simul/main.py
+++ CDA_simul/main.py
@@ -1,8 +1,20 @@
-# Copyright 2023 ETRI. All rights reserved. 
-# License-identifier: MIT
+# Copyright 2023 ETRI. 
+# License-identifier:GNU General Public License v3.0 or later
 # kwmin92@etri.re.kr, yssong00@etri.re.kr
 
-""" Main """
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published 
+# by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. 
+# If not, see <https://www.gnu.org/licenses/>.
+
+""" Main for Driving Negotiation Simulation"""
+
 import sys
 import os
 import pyautogui
